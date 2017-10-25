5de0250ce1d93818473667d5e28796f9fb2d25cc
Jure Šorn <sornjure@gmail.com>
2017-10-26 01:50:29 +0200

    Two rings

--- parse.py
+++ parse.py
@@ -7,43 +7,57 @@ import math
 import sys
 import re
 
-HEAD = "<html>\n<svg>\n"
-TAIL = "\n</svg>\n</html>"
+BASE = 0.75
+HEAD = '<html>\n<svg height=300px width=300px>\n<g transform="translate(150, 150), scale({})")>\n'.format(BASE)
+TAIL = "\n</g>\n</svg>\n</html>"
 
-base = 50
-aaa = [[0, 12], [10, 60], [5, 120]]
+ELEMENTS = [100, [0, 12, 1], [10, 60, 1], [5, 120, 1]]
+ELEMENTS_B = [90, [30, 12, 5]]
 
 def main():
     out = HEAD
+    out += get_group(ELEMENTS)
+    out += get_group(ELEMENTS_B)
+    print(out+TAIL)
+
+
+def get_group(elements):
+    if len(elements) < 2:
+        return
+    out = ""
     filled_positions = set()
-    for a in aaa:
-        length = a[0]
-        n = a[1]
+    offset = elements[0]
+    elements = elements[1:]
+    for element in elements:
+        length = element[0]
+        n = element[1]
+        width = element[2]
         positions = get_positions(n)
         positions = positions.difference(filled_positions)
-        out += get_circle(positions, base, base+length)
+        out += get_circle(positions, offset, offset-length, width)
         filled_positions.update(positions)
-    print(out+TAIL)
+    return out
 
 
 def get_positions(n):
     return set([i/n for i in range(n)])
 
 
-def get_circle(positions, ri, ro):
+def get_circle(positions, ro, ri, width):
     out = ""
     for position in positions:
         deg = position * 2*math.pi
-        out += get_line(deg, ri, ro)
+        out += get_line(deg, ri, ro, width)
     return out 
 
 
-def get_line(deg, ro, ri):
+def get_line(deg, ri, ro, width):
     x1 = math.cos(deg) * ri
     x2 = math.cos(deg) * ro
     y1 = math.sin(deg) * ri
     y2 = math.sin(deg) * ro
-    return '<line x1={} y1={} x2={} y2={} style="stroke-width:1; stroke:#000000"></line>'.format(x1, y1, x2,y2)
+    return '<line x1={} y1={} x2={} y2={} style="stroke-width:{}; ' \
+           'stroke:#000000"></line>'.format(x1, y1, x2, y2, width)
 
 
 if __name__ == '__main__':
